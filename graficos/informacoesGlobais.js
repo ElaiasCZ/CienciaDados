19df61ce6ee61227781b7e0e82818648f5b210a5
ElaiasCZ <108530713+ElaiasCZ@users.noreply.github.com>
2024-10-21 11:04:50 -0300

    save

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -4,7 +4,14 @@ async function vizualizarInformacoesGlobais() {
     const res = await fetch(url)
     const dados = await res.json()
     console.log(dados);
+    const paragrafo = document.createElement('p')
+    paragrafo.classList.add('graficos-container__texo')
+    paragrafo.innerHTML = `Você sabia que o mundo tem ${dados.total_pessoas_mundo} de pessoas e que aproximadamente ${dados.total_pessoas_conectadas} 
+    estam conectadas em alguma rede social e passam em média ${dados.tempo_medio} horas conectadas.`
+    console.log(paragrafo);
     
+    const container = document.getElementById('graficos-container')
+    container.appendChild(paragrafo)
 }
 
 vizualizarInformacoesGlobais
